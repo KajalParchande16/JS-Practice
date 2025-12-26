61b77041456dc051a4a0a341076904cabc372972
KajalParchande16 <parchandekajal16@gmail.com>
2025-12-26 11:04:46 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+// console.log(0>1);
+// console.log(0>=1);
+// console.log(0<1);
+// console.log(0<=1);
+// console.log(0==1);
+// console.log(0!=1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// In js equality == & coamparision < > <= >= treating as different
+// In comparision  convert null to number 0>=0=>true
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+
+
+
+
+
