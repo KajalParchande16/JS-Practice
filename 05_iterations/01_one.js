37c0c84962e1e9e54ccfd9dad226f30833b2c528
KajalParchande16 <parchandekajal16@gmail.com>
2025-12-31 06:21:34 +0000

    for loop break & continue

--- 05_iterations/01_one.js
+++ 05_iterations/01_one.js
@@ -9,12 +9,36 @@ for (let i = 1; i < 10; i++) {
 
 }
 
-for (let i = 1; i < 10; i++) {
+for (let i = 1; i <= 10; i++) {
     // const element = i;
     // console.log("outer Loop", i);
 
-    for (let j = 1; j < 10; j++) {
+    for (let j = 1; j <= 10; j++) {
         // console.log("Inner Loop", j);
         console.log(i + '*' + j + ' = ' + i * j);
     }
+}
+
+let myTeam = ['Harman', 'Smriti', 'Shaifali', 'Deepti', 'Jemi'];
+for (let i = 0; i < myTeam.length; i++) {
+    const element = myTeam[i];
+    console.log(element);
+
+}
+
+for (let i = 0; i <= 10; i++) {
+    if (i == 5) {
+        console.log(`detected ${i}`);
+        break;
+    }
+    console.log(`value of i is ${i}`)
+}
+// break it will break the execution immediatly
+
+for (let i = 0; i <= 10; i++) {
+    if (i == 5) {
+        console.log(`detected ${i}`);
+        continue;
+    }
+    console.log(`value of i is ${i}`)
 }
