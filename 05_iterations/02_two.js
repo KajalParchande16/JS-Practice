25856eb81d8b67a1fa88eee0ed3f95c47bc292a9
KajalParchande16 <parchandekajal16@gmail.com>
2025-12-31 10:11:20 +0000

    whil & do while loop

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,21 @@
+let val=1;
+while(val<=10){
+    console.log(val);
+    val=val+2;
+}
+
+let myTeam=['Harman','Smriti','Jemi','Deepti','Sneh'];
+
+let ar=0;
+while(ar < myTeam.length)
+{
+    console.log(myTeam[ar]);
+    ar=ar+1;
+}
+
+let score=11;
+do {
+    console.log(score);
+    score++;
+    
+} while (score<=10);
