8bfd0fcc32d09198bf441a68713852ace751f535
KajalParchande16 <parchandekajal16@gmail.com>
2025-12-31 04:44:31 +0000

    switch case

---
+++ 04_control_flow/02_switchCase.js
@@ -0,0 +1,21 @@
+// switch case
+
+const month=3;
+switch(month)
+{
+    case 1:
+        console.log(1);
+        break;
+    case 2:
+        console.log(2);
+        break;
+    case 3:
+        console.log(3);
+        break;
+     default:
+        console.log("default")
+        break;
+
+}
+
+// break used to break the statement conditionexpect default
