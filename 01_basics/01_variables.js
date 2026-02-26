96cb6cab2eeac197d5c6cd1ffeb7f19dbba75c76
KajalParchande16 <parchandekajal16@gmail.com>
2026-02-26 11:28:07 +0000

    variable basic

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -11,3 +11,25 @@ prefer not to use var due to block scope & functional scope
 */
 console.log(accountNumber);
 console.table([name,accountNumber,accountState,city,stateName]);
+
+if(true)
+{
+    var a=20;
+}
+console.log(a);
+
+for (var i = 0; i < 5; i++) {
+    
+}
+console.log(i);
+let j = 1;
+while (j <= 3) {
+  var x = j * 2;
+  console.log(x);
+  j++;
+}
+console.log(x);
+let b=10;
+let c=b;
+b=20;
+console.log(b,c);
