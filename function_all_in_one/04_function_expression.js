0a4d45ee43db930fd4aa7d0654773965a44a2718
KajalParchande16 <parchandekajal16@gmail.com>
2026-02-28 04:34:14 +0000

    function expression

---
+++ function_all_in_one/04_function_expression.js
@@ -0,0 +1,14 @@
+// console.log(addNum(10,20));
+// we can't access function expression befoer intitalization
+const addNum=function(n1,n2)
+{
+    console.log(n1+n2);
+}
+console.log(addNum(10,20));
+
+
+console.log(addingNo(35,20));
+function addingNo(a,b)
+{
+    console.log(a+b);
+}
