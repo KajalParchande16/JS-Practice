0da28707cd44cd198d5c6e8403a9e4320fbd2a53
KajalParchande16 <parchandekajal16@gmail.com>
2026-03-02 10:53:52 +0000

    searching array

---
+++ Array_all_in_one/04_searching_array.js
@@ -0,0 +1,6 @@
+let arr=["Sonal","Kajal","Prerna","Maitreyi","Kajal"];
+
+console.log(arr.indexOf('Kajal'));
+console.log(arr.lastIndexOf("Kajal"));
+console.log(arr.includes('Prathna'));
+console.log(arr.indexOf("Prathna"));
