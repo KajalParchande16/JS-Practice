212c46407c7237eee4e2fc3dfc8faaa8d5bb78a2
KajalParchande16 <parchandekajal16@gmail.com>
2026-03-02 10:23:53 +0000

    array to string

---
+++ Array_all_in_one/03_arra_to_string.js
@@ -0,0 +1,6 @@
+let arr=["Kajal","Prerna","Maitreyi","Sonal","Prathna"];
+console.log(arr);
+console.log(arr.join(","));
+console.log(arr.join("_"));
+console.log(arr.join(" "));
+console.log(arr.join(" | "));
