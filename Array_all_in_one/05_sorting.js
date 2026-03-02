6b78a2f947e0ecd6c3a298bbeeb6b74346277c03
KajalParchande16 <parchandekajal16@gmail.com>
2026-03-02 11:50:58 +0000

    array sorting

---
+++ Array_all_in_one/05_sorting.js
@@ -0,0 +1,14 @@
+let arr=["Sonal","Arpita","Divya","Charu","Kajal","Praju","Radha"];
+console.log(arr.sort());
+console.log(arr.reverse());
+let arr2=[100,23,56,38,66,30,18,22,116,94,45];
+console.log(arr2.sort());//this will not work properly
+// because it check like this "100" ,"23"=>1st element 100=>"1" is smaller tahen "2"
+
+console.log(arr2.sort((a,b)=>a-b)); //assending order
+// let a=100;
+// let b=23;
+// 100-23=>77 +ve the it will return 2nd element 23
+// 23-56=>-33 -ve no so it will return 1st element 23
+
+console.log(arr2.sort((a,b)=>b-a)); //decending order
