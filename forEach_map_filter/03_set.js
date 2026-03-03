17653eb4f5e7911ba7d572c11f0457d3421c5907
KajalParchande16 <parchandekajal16@gmail.com>
2026-03-03 12:19:20 +0000

    loops pratice

--- forEach_map_filter/03_set.js
+++ forEach_map_filter/03_set.js
@@ -12,10 +12,22 @@ console.log(arr1);
 let users=["Kajal","Admin","Saniya","Admin","Riya","Kajal"];
 let s1=new Set(users); //but set give us data as object format
 console.log(s1);
+for (const element of s1) {
+    console.log("element",element);
+    
+}
+
 // but we need it in array format
 let s2=[...new Set(users)];
 console.log(s2);
-
+for (const key in s2) {
+    console.log("keys",s2[key])
+    
+}
+for (const element of s2) {
+    console.log("s2",element);
+    
+}
 let s3=new Map([
     [ "Kajal",18],
     [2,"Sneh"],
