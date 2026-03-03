f8d781a35d035009780fc59f53ad9081ed8bd0b9
KajalParchande16 <parchandekajal16@gmail.com>
2026-03-03 10:50:35 +0000

    set & map

---
+++ forEach_map_filter/03_set.js
@@ -0,0 +1,25 @@
+let arr=[10,15,20,10,25,30,15,20];
+let arr1=new Set(arr);
+console.log(arr1);
+arr1.add(40);
+console.log(arr1);
+arr1.delete(30);
+console.log(arr1);
+arr1.clear();
+console.log(arr1);
+
+
+let users=["Kajal","Admin","Saniya","Admin","Riya","Kajal"];
+let s1=new Set(users); //but set give us data as object format
+console.log(s1);
+// but we need it in array format
+let s2=[...new Set(users)];
+console.log(s2);
+
+let s3=new Map([
+    [ "Kajal",18],
+    [2,"Sneh"],
+    [true,23],
+    [10,20,30,"Harman"]
+]);
+console.log(s3);
