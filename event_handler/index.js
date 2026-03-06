16f655a9106256ec8b9295180133818ec3f00ddc
KajalParchande16 <parchandekajal16@gmail.com>
2026-03-06 07:14:57 +0000

    basic event handler

---
+++ event_handler/index.js
@@ -0,0 +1,19 @@
+// function eventHandler()
+// {
+//     const h1=document.getElementById('indw');
+//     console.log(h1);
+//     h1.textContent="Ind vs Aus Test Match";
+// }
+
+    const element=document.getElementById('indw');
+    element.onclick=function eventHandler()
+    {
+        element.textContent="Ind vs Aus Test Match"
+    }
+    element.onclick=function eventHandler()
+    {
+        element.style.backgroundColor="red";
+    }
+
+    
+
