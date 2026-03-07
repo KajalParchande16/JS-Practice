4b8b9f497d60434a9022bfc3f1c14a2c84c6cea7
KajalParchande16 <parchandekajal16@gmail.com>
2026-03-07 07:14:42 +0000

    math random method  practice

---
+++ Maths_all_in_one/1.basic.js
@@ -0,0 +1,27 @@
+// console.log(Math.abs(-6));
+// console.log(Math.LN10);
+// console.log(Math.PI);
+// console.log(Math.ceil(5.6));
+// console.log(Math.floor(5.6));
+
+// console.log("Math Random ");
+
+// console.log(Math.random());
+// console.log(Math.random()*10); //0-9
+// console.log(Math.random()*10+1);
+// console.log(Math.floor(Math.random()*10)+1);//1-10
+
+// print no 1-6
+// Math.floor(Math.random()*totalNoOfOutCome)+shift
+
+// Math.floor(Math.random())(max-min+1)+shift
+// 25-15=>10+1=>11
+// console.log(Math.floor(Math.random()*6)+1);
+// console.log(Math.floor(Math.random()*10));
+
+console.log(Math.floor(Math.random()*11)+15)
+
+
+
+
+
