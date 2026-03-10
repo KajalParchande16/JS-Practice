c36a27878dc64f340c974c134f83acd079bb9eeb
KajalParchande16 <parchandekajal16@gmail.com>
2026-03-10 08:16:31 +0000

    window object

---
+++ Advance_Concept/02_window_object.js
@@ -0,0 +1,14 @@
+// console.log(window)
+// browser global object is window
+// but not for node environment
+// node env->global obje is blobal
+
+// global Object=> In browser it is called window but in node env it is called global
+// globalThis =>global object of the node env
+
+// console.log(global);
+
+console.log(globalThis);
+
+// this keyword =>{}(empty object) in node env,window object in browser
+console.log(this);
