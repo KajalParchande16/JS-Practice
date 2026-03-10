e947bb10bab56343324c7148677adc973d324b69
KajalParchande16 <parchandekajal16@gmail.com>
2026-03-10 06:51:15 +0000

    use scrict

---
+++ Advance_Concept/01_usestrict.js
@@ -0,0 +1,11 @@
+'use strict'
+var a=10;
+b=20;
+console.log(a,b);
+
+function user(name,name)
+{
+    console.log(name,name);
+}
+user("Rohit","Mohit");//it will give Mohit Mohit 
+// 2nd one will override 1st name
