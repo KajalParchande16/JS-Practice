b29f49944da026f93cb0b97d6be5017f55823c2e
KajalParchande16 <parchandekajal16@gmail.com>
2026-03-10 06:52:05 +0000

    solution

--- Advance_Concept/01_usestrict.js
+++ Advance_Concept/01_usestrict.js
@@ -1,11 +1,11 @@
 'use strict'
 var a=10;
-b=20;
+var b=20;
 console.log(a,b);
 
-function user(name,name)
+function user(name1,name2)
 {
-    console.log(name,name);
+    console.log(name1,name2);
 }
 user("Rohit","Mohit");//it will give Mohit Mohit 
 // 2nd one will override 1st name
