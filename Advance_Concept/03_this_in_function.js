baaddfa27d6f2b6c8d7cd0f0c8125a794afd3641
KajalParchande16 <parchandekajal16@gmail.com>
2026-03-10 10:45:22 +0000

    what is need of this in object

--- Advance_Concept/03_this_in_function.js
+++ Advance_Concept/03_this_in_function.js
@@ -1,13 +1,21 @@
- const user= {
-name:"Rohit",
-age:20,
-greet:function()
-{
-    console.log(`hi ${this.name}`);
-}
+const user = {
+    name: "Rohit",
+    age: 20,
+    greet: function () {
+        console.log(`hi ${user.name}`);
+        // if we pass user.name (hard code value) 
+        // if we pass this function to another object it will take same value 
+    }
 }
 
 // this ==user;
 // this refer to user object
 
 user.greet();
+const user2 = {
+    name: "Mogit",
+    age: 24
+}
+
+user.greet=user.greet;
+console.log(user.greet());
