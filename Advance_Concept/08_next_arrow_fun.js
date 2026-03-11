35d47afd0b3765d6da0c4fe223e3cbd1b568ce41
KajalParchande16 <parchandekajal16@gmail.com>
2026-03-11 05:40:10 +0000

    example of why this not work in arrow function

---
+++ Advance_Concept/08_next_arrow_fun.js
@@ -0,0 +1,15 @@
+
+const user = {
+    name: 'Rakesh',
+    age: 23,
+
+    greet: function () {
+        console.log("grret function ", this);
+        function meet() {
+            console.log(this); //this will point to user object =>user invoked greet
+        }
+        meet(); //this point to global object
+        // as we know if function invoked itself withou strict mode it will point global object
+    }
+}
+user.greet();
